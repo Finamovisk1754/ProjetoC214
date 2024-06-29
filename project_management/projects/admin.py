41c798de778a7f5867c15416bf9a157fd321edb7
Finamovisk1754 <163656853+Finamovisk1754@users.noreply.github.com>
2024-06-28 21:50:54 -0300

    Update admin.py

--- project_management/projects/admin.py
+++ project_management/projects/admin.py
@@ -1,19 +1,16 @@
+# admin.py
 from django.contrib import admin
-from .models import Project, Task
-
-class TaskInline(admin.TabularInline):
-    model = Task
-    extra = 1
+from .models import Project, ResponseFile, Task
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'owner', 'created_at', 'updated_at')
-    search_fields = ('name', 'owner__username')
-    list_filter = ('created_at', 'updated_at')
-    inlines = [TaskInline]
+    list_display = ('name', 'description', 'owner', 'created_at', 'updated_at')
+
+@admin.register(ResponseFile)
+class ResponseFileAdmin(admin.ModelAdmin):
+    list_display = ('project', 'file', 'uploaded_at')
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('name', 'project', 'assignee', 'due_date', 'completed', 'created_at', 'updated_at')
-    search_fields = ('name', 'project__name', 'assignee__username')
-    list_filter = ('due_date', 'completed', 'created_at', 'updated_at')
+    list_display = ('name', 'description', 'project', 'created_at', 'updated_at', 'completed')
+    list_filter = ('project', 'completed')
